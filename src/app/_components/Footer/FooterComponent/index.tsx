c35308fd5b53a1891b7bc9a337eacfdd5bd80002
Masoud61UI <masoud6181@gmail.com>
2024-07-03 00:34:56 +0330

    add footer

---
+++ src/app/_components/Footer/FooterComponent/index.tsx
@@ -0,0 +1,73 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { inclusions, noHeaderFooterUrls, profileNavItems } from '../../../constants'
+import { Gutter } from '../../Gutter'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FooterProps, Media } from '../../../../payload/payload-types'
+import { Button } from '../../Button'
+
+export default function FooterComponent({ footer }: { footer: FooterProps }) {
+  const pathname = usePathname()
+
+  const navItems = footer?.navItems || []
+
+  return (
+    <footer className={noHeaderFooterUrls.includes(pathname) ? 'hidden' : ''}>
+      <Gutter>
+        <ul className="grid justify-center gap-6 p-0 md:grid-cols-2 sm:gap-4 sm:margin-y-6 lg:grid-cols-1">
+          {inclusions.map(inclusion => (
+            <li key={inclusion.title}>
+              <Image
+                src={inclusion.icon}
+                alt={inclusion.title}
+                width={36}
+                height={36}
+                className="mb-4"
+              />
+              <h5>{inclusion.title}</h5>
+              <p>{inclusion.description}</p>
+            </li>
+          ))}
+        </ul>
+      </Gutter>
+
+      <div className="py-12 sm:py-6 bg-black">
+        <Gutter>
+          <div className="flex justify-between items-center flex-wrap gap-5 sm:mt-6 sm:flex-col sm:justify-center sm:text-center">
+            <Link href={'/'} className="text-2xl text-white font-bold">
+              MB eShop
+            </Link>
+
+            <p className="text-white">{footer.copyright}</p>
+
+            <div className="flex gap-5 pb-3">
+              {navItems.map(item => {
+                const icon = item?.link?.icon as Media
+
+                return (
+                  <Button
+                    key={item.link.label}
+                    el="link"
+                    href={item.link.url}
+                    newTab={true}
+                    className="w-full"
+                  >
+                    <Image
+                      src={icon?.url}
+                      alt={item.link.label}
+                      width={24}
+                      height={24}
+                      className="w-6 h-6"
+                    />
+                  </Button>
+                )
+              })}
+            </div>
+          </div>
+        </Gutter>
+      </div>
+    </footer>
+  )
+}
